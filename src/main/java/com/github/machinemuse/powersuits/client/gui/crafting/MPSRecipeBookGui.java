bf2f60886e1e6d24296170ccc461435542fa5732
lehjr <lehjr1@gmail.com>
2019-11-27 12:21:17 -0500

    fixed year formatting

--- src/main/java/com/github/machinemuse/powersuits/client/gui/crafting/MPSRecipeBookGui.java
+++ src/main/java/com/github/machinemuse/powersuits/client/gui/crafting/MPSRecipeBookGui.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) ${DATE} MachineMuse, Lehjr
+ * Copyright (c) 2019 MachineMuse, Lehjr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
